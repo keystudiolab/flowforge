6cc49e13ab6cfa007a064efb16af27a60b3e51cd
Egor <Jok@Olesas-MacBook-Air.local>
2026-07-30 23:36:36 +0300

    Add Google Analytics tracking

--- src/app/(ru)/layout.tsx
+++ src/app/(ru)/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
@@ -97,7 +98,9 @@ export default function RootLayout({
       lang="ru"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}
+        <GoogleAnalytics gaId="G-73ZYKL86LS" />
+      </body>
     </html>
   );
 }
